fc461b2d18bcb0ee6b9eaa7f3c6b33bda6715081
Sam Magura <srmagura@gmail.com>
2026-07-20 16:54:16 -0400

    gemini api working

--- SentenceCardAI/main.ts
+++ SentenceCardAI/main.ts
@@ -11,13 +11,17 @@ const apiKeys: ApiKeys = JSON.parse(
   await readFile(path.join(os.homedir(), 'AI_API_KEYS.json'), 'utf-8'),
 );
 
+const systemInstructions = await readFile('SystemInstructions.txt', 'utf-8');
+
 const ai = new GoogleGenAI({
   apiKey: apiKeys.gemini,
 });
 
 const interaction = await ai.interactions.create({
   model: 'gemini-3.5-flash',
-  input: 'Explain how AI works in less than 20 words',
+  system_instruction: systemInstructions,
+  input: '学校',
+  // TODO change thinking level? https://ai.google.dev/gemini-api/docs/text-generation
 });
 
 console.log(interaction.output_text);
