d1df26844c929045c1537cf22c34d45eb1612d6c
Sam Magura <srmagura@gmail.com>
2026-07-24 08:03:45 -0400

    wip

---
+++ SentenceCardAI/src/hoshiReaderCardCompleter.ts
@@ -0,0 +1,90 @@
+import OpenAI from 'openai';
+import { apiKeys } from './apiKeys.ts';
+import { YankiConnect } from 'yanki-connect';
+
+const AI_MODEL = 'gpt-5.6-luna';
+
+const INSTRUCTIONS = `
+I will send a Japanese sentence. The target word is in an HTML <b> tag. I want to learn the target word.
+
+You must output 3 lines:
+
+1. The sentence in Japanese, with furigana inside square brackets. The furigana must appear after the word. For each word that requires furigana, insert a single space before the word. Wrap the target word in HTML <b></b>, including its furigana and square brackets.
+2. An English translation of the example sentence.
+3. Blank line
+`.trim();
+
+const anki = new YankiConnect();
+const noteIds = await anki.note.findNotes({
+  query: `"deck:Sentence Mining::Text Sentence Mining" tag:hoshi-reader-new`,
+});
+
+if (noteIds.length > 20) {
+  throw new Error(
+    'too many note ids (this can certainly be improved with batching)',
+  );
+}
+
+const noteInfo = await anki.note.notesInfo({ notes: noteIds });
+
+const sentKanji = noteInfo[0]!.fields.SentKanji?.value.trim();
+if (!sentKanji) {
+  console.log(
+    `SentKanji is undefined/empty for note ${noteIds[0]}, skipping...\n`,
+  );
+  throw new Error('temp error');
+}
+
+const openai = new OpenAI({
+  apiKey: apiKeys.openai,
+});
+
+const startTime = performance.now();
+
+const response = await openai.responses.create({
+  model: AI_MODEL,
+  instructions: INSTRUCTIONS,
+  input: sentKanji,
+});
+
+console.log(response.output_text + '\n\n\n');
+
+const lines = response.output_text.trim().split('\n');
+
+const linesPerWord = 3;
+
+for (let i = 0; i < lines.length; i += linesPerWord) {
+  const sentFurigana = lines[i]!.trim();
+  const sentEng = lines[i + 1]!.trim();
+  // lines[i + 2] is blank
+
+  if (!sentFurigana || !sentEng) {
+    throw new Error('ERROR: Failed to parse output_text!');
+  }
+
+  console.log({ sentFurigana, sentEng });
+
+  try {
+    // const noteId = await anki.note.addNote({
+    //   note: {
+    //     deckName: DECK_NAME,
+    //     modelName: 'Japanese sentences+',
+    //     fields: {
+    //       SentKanji: sentenceData.sentKanji,
+    //       SentFurigana: sentenceData.sentFurigana,
+    //       sentEng: sentenceData.sentEng,
+    //     },
+    //     tags: ['sentence-card-ai', AI_MODEL],
+    //   },
+    // });
+    // console.log(`Success! Card created with Note ID: ${noteId}`);
+  } catch (error) {
+    // YankiConnect automatically extracts the API error string and throws it
+    console.error('Failed to add card:', error);
+  }
+
+  console.log();
+}
+
+const elapsedSeconds = Math.round((performance.now() - startTime) / 1000);
+console.log(`Took ${elapsedSeconds} seconds`);
--- SentenceCardAI/src/wordListCardGenerator.ts
+++ SentenceCardAI/src/wordListCardGenerator.ts
@@ -2,7 +2,7 @@ import { GoogleGenAI } from '@google/genai';
 import { readFile } from 'fs/promises';
 import path from 'path';
 import { YankiConnect } from 'yanki-connect';
-import { apiKeys } from './apiKeys.js';
+import { apiKeys } from './apiKeys.ts';
 
 // TODO Use 3.5-flash-lite?
 const AI_MODEL = 'gemini-3.6-flash';
@@ -10,10 +10,23 @@ const DECK_NAME = 'Sentence Mining::Text Sentence Mining';
 const WORD_LIST_DIR = 'wordLists/soundEuphonium';
 const EPISODE = 's1e03'; // TODO make command line arg
 
+const INSTRUCTIONS = `
+I will send multiple Japanese words, one on each line. These are the "target words".
+
+For each target word, create a SIMPLE example sentence in Japanese to help me learn the target word. Apart from the target word, the sentence should use easy vocab and kanji.
+
+You must output 4 lines for each target word:
+
+1. Example sentence in Japanese (no furigana). Wrap the target word in HTML <b></b>.
+2. Example sentence in Japanese, with furigana inside square brackets. For each word that requires furigana, insert a single space before the word. Wrap the target word in HTML <b></b>, including its furigana and square brackets.
+3. An English translation of the example sentence.
+4. Blank line
+
+Do NOT create a sentence that includes multiple target words.
+`.trim();
+
 const vocabFilename = path.join(WORD_LIST_DIR, `${EPISODE}.txt`);
 const vocabFileContent = await readFile(vocabFilename, 'utf-8');
-
-const systemInstructions = await readFile('SystemInstructions.md', 'utf-8');
 
 const ai = new GoogleGenAI({
   apiKey: apiKeys.gemini,
@@ -21,7 +34,7 @@ const ai = new GoogleGenAI({
 
 const interaction = await ai.interactions.create({
   model: AI_MODEL,
-  system_instruction: systemInstructions,
+  system_instruction: INSTRUCTIONS,
   input: vocabFileContent,
 });
 
@@ -34,13 +47,14 @@ console.log('\n\n\n');
 
 const anki = new YankiConnect();
 
+// TODO this interface is actually unnecessary
 interface SentenceData {
   sentKanji: string;
   sentFurigana: string;
   sentEng: string;
 }
 
-const lines = interaction.output_text.split('\n');
+const lines = interaction.output_text.trim().split('\n');
 
 const linesPerWord = 4;
 
