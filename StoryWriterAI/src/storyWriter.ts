a6faf1495c10a334887f2ff690b847035a2507bf
Sam Magura <srmagura@gmail.com>
2026-07-27 20:01:28 -0400

    story writer

--- StoryWriterAI/src/storyWriter.ts
+++ StoryWriterAI/src/storyWriter.ts
@@ -5,15 +5,17 @@ import { apiKeys } from './apiKeys.ts';
 
 // Edit these each time
 const LEVEL = 'N4';
-const STORY_NAME = `[${LEVEL}] Teasing Master Takagi-san`;
-const STORY_DESC = `Fanfic of Teasing Master Takagi-san (anime)`;
-const NUMBER_OF_CHAPTERS = 5;
+const STORY_NAME = `[${LEVEL}] Sound! Euphonium`;
+const STORY_DESC = `Fanfic of Sound! Euphonium (anime)`;
+const NUMBER_OF_CHAPTERS = 7;
 const CHAPTER_LENGTH = '300 - 450 words';
 
 const INSTRUCTIONS = `
-Write a story in Japanese. Use only N4 vocab or easier. No furigana.
+Write a story in Japanese. Use only ${LEVEL} vocab or easier. No furigana.
 
 For all character names, **only** use their given name. Write all character names in kana wrapped in a <span> tag. Examples: <span>はると</span>は学生です, <span>ジョン</span>は学生です
+
+All narration and prose must be in short form (write だ instead of です).
 
 The story may have multiple chapters.
 `.trim();
