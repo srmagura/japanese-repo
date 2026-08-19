57f962469722ad05f0e01d53474112d11041ce21
Sam Magura <srmagura@gmail.com>
2026-08-19 10:49:21 -0400

    Improve koini script

--- koini.ps1
+++ koini.ps1
@@ -1,3 +1,6 @@
-Start-Process "ms-clock:"
+# Can't figure out how to hide the output from these
 Start-Process -FilePath "C:\Users\SamMa\Documents\CTorrents\CVisual\Love's Sweet Garnish\KoiniKanmiwoSoete.exe" -WorkingDirectory "C:\Users\SamMa\Documents\CTorrents\CVisual\Love's Sweet Garnish"
 Start-Process -FilePath "C:\Program Files\LunaTranslator_x64\LunaTranslator.exe" -WorkingDirectory "C:\Program Files\LunaTranslator_x64"
+
+Start-Sleep -Seconds 10
+Write-Host "`n`n`n`n✅ Habit complete!"
