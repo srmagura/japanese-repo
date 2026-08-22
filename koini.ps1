39f9f520abbe5e74530a4ea76113516fd82cad96
Sam Magura <srmagura@gmail.com>
2026-08-22 13:41:14 -0400

    fix koini

--- koini.ps1
+++ koini.ps1
@@ -2,5 +2,5 @@
 Start-Process -FilePath "C:\Users\SamMa\Documents\CTorrents\CVisual\Love's Sweet Garnish\KoiniKanmiwoSoete.exe" -WorkingDirectory "C:\Users\SamMa\Documents\CTorrents\CVisual\Love's Sweet Garnish"
 Start-Process -FilePath "C:\Program Files\LunaTranslator_x64\LunaTranslator.exe" -WorkingDirectory "C:\Program Files\LunaTranslator_x64"
 
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 120
 Write-Host "`n`n`n`n✅ Habit complete!"
